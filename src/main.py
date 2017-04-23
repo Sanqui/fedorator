6c995259ccbd41af6fde29d13bf06b82d21bc987
Sanqui <gsanky@gmail.com>
2017-04-23 19:17:15 +0200

    Show connected USB storage on screen

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ObjectProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, StringProperty, ObjectProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
+import usbdisks
+
 class FedoratorMenu(Widget):
-    pass
+    disk_text = StringProperty()
+    
+    def update_disks(self, dt):
+        disks = usbdisks.get_usb_disks()
+        self.disk_text = ", ".join(disk.dev_name for disk in disks)
 
 class FedoratorApp(App):
     def build(self):
         menu = FedoratorMenu()
+        Clock.schedule_interval(menu.update_disks, 1.0)
         return menu
 
 
