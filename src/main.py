55391aa95ed7cb0655f2c3bc047c3d280d4430ec
Sanqui <gsanky@gmail.com>
2017-04-24 10:51:55 +0200

    Show IP on screen

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, StringProperty, ObjectProperty, ReferenceListProperty
@@ -10,6 +12,8 @@ class FedoratorMenu(Widget):
     left_disk_text = StringProperty()
     right_disk_text = StringProperty()
     
+    ip = StringProperty()
+    
     def update_disks(self, dt):
         disks = usbdisks.get_usb_disks()
         disk_texts = ["", ""]
@@ -18,11 +22,15 @@ class FedoratorMenu(Widget):
             disk_texts.insert(0, text)
         self.left_disk_text = disk_texts[0]
         self.right_disk_text = disk_texts[1]
+    
+    def update_ip(self, dt):
+        self.ip = subprocess.getoutput('hostname -I')
 
 class FedoratorApp(App):
     def build(self):
         menu = FedoratorMenu()
         Clock.schedule_interval(menu.update_disks, 1.0)
+        Clock.schedule_interval(menu.update_ip, 2.0)
         return menu
 
 
