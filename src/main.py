6da6c8084edc4e732cda45bb85129af72d43a930
Sanqui <gsanky@gmail.com>
2017-04-24 15:00:53 +0200

    React on touch

--- src/main.py
+++ src/main.py
@@ -2,17 +2,28 @@ import subprocess
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, StringProperty, ObjectProperty, ReferenceListProperty
+from kivy.properties import BooleanProperty, NumericProperty, StringProperty, ObjectProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 
 import usbdisks
 
+class FlashMenu(Widget):
+    pass
+
 class FedoratorMenu(Widget):
     left_disk_text = StringProperty()
     right_disk_text = StringProperty()
     
+    status_message = StringProperty()
+    
     ip = StringProperty()
+    
+    ready = BooleanProperty()
+    
+    def on_touch_up(self, touch):
+        if self.ready:
+            self.status_message="Touched"
     
     def update_disks(self, dt):
         disks = usbdisks.get_usb_disks()
@@ -20,11 +31,20 @@ class FedoratorMenu(Widget):
         for disk in disks[0:2]:
             text = "{}: {:.3} GiB".format(disk.dev_name, disk.size.bytes/1024/1024/1024)
             disk_texts.insert(0, text)
+
         self.left_disk_text = disk_texts[0]
         self.right_disk_text = disk_texts[1]
+        
+        if disks:
+            self.status_message = "Tap to begin"
+            self.ready = True
+        else:
+            self.status_message = "Please insert flash"
+            self.ready = False
     
     def update_ip(self, dt):
         self.ip = subprocess.getoutput('hostname -I')
+    
 
 class FedoratorApp(App):
     def build(self):
